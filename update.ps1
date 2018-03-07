c749693dd0edbe3ebc49f1e5d4aae20e691cf567
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2018-03-07 13:01:52 -0500

    .\update.ps1 no longer recursive by default.

--- update.ps1
+++ update.ps1
@@ -1,5 +1,9 @@
-PARAM([Switch]$Force)
+﻿PARAM([Switch]$Force,[Switch]$Recursive)
 If(-not $Force.IsPresent) {Write-verbose -verbose "Update cancelled. Run update.ps1 -force to update submodules."}
 Write-verbose -verbose "update.ps1 fetches the current commits of the submodules. THIS MAY BREAK THE MODULE and constitutes a change as far as git is concerned."
-If($Force.IsPresent) {Write-verbose -verbose "Updating submodules..."
+If($Force.IsPresent -and $Recursive.IsPresent) {
+Write-verbose -verbose "Updating submodules recursively..."
 git submodule update --recursive --remote }
+ELSEIf($Force.IsPresent) {
+Write-verbose -verbose "Updating submodules..."
+git submodule update --remote }
